df0de0ff44ae09f8843a999e197b0bf51b27a29f
skyjiang <470623352@qq.com>
2024-02-22 17:56:00 +0800

    polish http_server

--- internal/server/http_server.go
+++ internal/server/http_server.go
@@ -1,17 +1,18 @@
 package server
 
 import (
-	"RedisShake/internal/config"
 	"RedisShake/internal/log"
 	"RedisShake/internal/reader"
 	"RedisShake/internal/status"
 	"RedisShake/internal/writer"
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"strconv"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
 )
 
@@ -20,10 +21,11 @@ type HttpServerOptions struct {
 }
 
 func StartHttpServer(opts *HttpServerOptions) *http.Server {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/submit", submitTask)
-	mux.HandleFunc("/cancel", cancelTask)
-	mux.HandleFunc("/display", displayTask)
+	mux := mux.NewRouter()
+	mux.HandleFunc("/msa/task/start", submitTask)
+	mux.HandleFunc("/msa/task/cancel/{id}", cancelTask)
+	mux.HandleFunc("/msa/task/info/{id}", displayTask)
+	mux.HandleFunc("/msa/task/health/{id}", healthCheck)
 
 	httpSvr := &http.Server{
 		Addr:         ":" + strconv.Itoa(opts.HttpPort),
@@ -47,7 +49,10 @@ type httpResponse struct {
 	Message interface{} `json:"message,omitempty"`
 }
 
-var successHttpResponse httpResponse = httpResponse{0, "success"}
+var (
+	successResp httpResponse = httpResponse{0, "success"}
+	noTaskResp httpResponse = httpResponse{-1, "not task"}
+)
 
 func submitTask(w http.ResponseWriter, r *http.Request) {
 	header := w.Header()
@@ -57,9 +62,8 @@ func submitTask(w http.ResponseWriter, r *http.Request) {
 
 	// 只能运行一个任务
 	if status.CurrentTask != nil {
-		if _, err := w.Write([]byte("exist running task")); err != nil {
-			log.Warnf("respone http err:%s", err.Error())
-		}
+		resp, _ := json.Marshal(httpResponse{-1, []byte("exist running task:" + status.CurrentTask.ID)})
+		_, _ = w.Write(resp)
 		return
 	}
 
@@ -76,7 +80,7 @@ func submitTask(w http.ResponseWriter, r *http.Request) {
 
 	// 创建并运行任务
 	var err error
-	status.CurrentTask, err = CreateAndStartTask(ctx, v)
+	status.CurrentTask, err = CreateAndStartTask(ctx, v, cancel)
 	if err != nil {
 		log.Warnf("read task info err:%s", err.Error())
 		resp, _ := json.Marshal(httpResponse{-1, err.Error()})
@@ -84,35 +88,42 @@ func submitTask(w http.ResponseWriter, r *http.Request) {
 		cancel()
 		return
 	}
-	status.CurrentTask.Cancel = cancel
 
-	resp, _ := json.Marshal(successHttpResponse)
+	resp, _ := json.Marshal(successResp)
 	if _, err := w.Write(resp); err != nil {
 		log.Warnf("respone http err:%s", err.Error())
 	}
 }
 
-func CreateAndStartTask(ctx context.Context, v *viper.Viper) (*status.SyncTask, error) {
+func CreateAndStartTask(ctx context.Context, v *viper.Viper, cancel context.CancelFunc) (*status.SyncTask, error) {
+	taskId := v.GetString("advanced.task_id")
+	if taskId == "" {
+		return nil, errors.New("task id is null")
+	}
+
 	theReader, err := reader.CreateReader(v)
 	if err != nil {
 		return nil, err
 	}
 
-	theWriter, err := writer.CreateWriter(v, config.Opt.Advanced)
+	theWriter, err := writer.CreateWriter(v)
 	if err != nil {
+		// TODO: reader释放不了
 		return nil, err
 	}
 
 	task := &status.SyncTask{
 		Ctx:    ctx,
 		V:      v,
+		ID:     taskId,
 		Writer: theWriter,
 		Reader: theReader,
+		Cancel: cancel,
 		Stat:   new(status.Stat),
 	}
 
 	task.Stat.Time = time.Now().Format("2006-01-02 15:04:05")
-	log.Infof("start syncing...")
+	log.Infof("task:[%s] start syncing...", taskId)
 
 	ch := theReader.StartRead(ctx)
 	go func() {
@@ -134,23 +145,27 @@ func cancelTask(w http.ResponseWriter, r *http.Request) {
 	header := w.Header()
 	header.Add("content-type", "application/json")
 
-	log.Infof("request cancel running task from:%s", r.RemoteAddr)
+	vars := mux.Vars(r)
+	taskId := vars["id"]
+	log.Infof("request cancel task:%s from:%s", taskId, r.RemoteAddr)
 
-	if status.CurrentTask == nil {
+	if status.CurrentTask == nil || status.CurrentTask.ID != taskId {
 		log.Infof("current not exist running task")
-		resp, _ := json.Marshal(httpResponse{0, "no task"})
+		resp, _ := json.Marshal(noTaskResp)
 		_, _ = w.Write(resp)
 		return
 	}
 
+	// cancel后会将reader相关的资源释放掉
 	status.CurrentTask.Cancel()
+	// 关闭writer相关的资源
 	if theWriter, ok := status.CurrentTask.Writer.(writer.Writer); ok {
 		theWriter.Close()
 	}
 	status.CurrentTask = nil
 	log.Infof("cancel running task success")
 
-	resp, _ := json.Marshal(successHttpResponse)
+	resp, _ := json.Marshal(successResp)
 	if _, err := w.Write(resp); err != nil {
 		log.Warnf("respone http err:%s", err.Error())
 	}
@@ -161,10 +176,12 @@ func displayTask(w http.ResponseWriter, r *http.Request) {
 	header := w.Header()
 	header.Add("content-type", "application/json")
 
-	log.Infof("display running task from:%s", r.RemoteAddr)
+	vars := mux.Vars(r)
+	taskId := vars["id"]
+	log.Infof("display task:%s from:%s", taskId, r.RemoteAddr)
 
-	if status.CurrentTask == nil {
-		resp, _ := json.Marshal(httpResponse{0, "no task"})
+	if status.CurrentTask == nil || status.CurrentTask.ID != taskId {
+		resp, _ := json.Marshal(noTaskResp)
 		_, _ = w.Write(resp)
 		return
 	}
@@ -177,6 +194,27 @@ func displayTask(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resp, _ := json.Marshal(response)
+	if _, err := w.Write(resp); err != nil {
+		log.Warnf("respone http err:%s", err.Error())
+	}
+}
+
+// 任务健康检查
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	header := w.Header()
+	header.Add("content-type", "application/json")
+
+	vars := mux.Vars(r)
+	taskId := vars["id"]
+	log.Infof("check task:%s from:%s", taskId, r.RemoteAddr)
+
+	if status.CurrentTask == nil || status.CurrentTask.ID != taskId {
+		resp, _ := json.Marshal(noTaskResp)
+		_, _ = w.Write(resp)
+		return
+	}
+
+	resp, _ := json.Marshal(successResp)
 	if _, err := w.Write(resp); err != nil {
 		log.Warnf("respone http err:%s", err.Error())
 	}
